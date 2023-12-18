58b92acdb4dc119ede41d0544a8d316327a22e79
Claudio9616 <clares9611@gmail.com>
2023-12-18 19:13:16 +0100

    finish

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+1 Recupero l'elemento dal DOM
+2 Chiedo quanti chilometri vuole percorrere l'utente
+3 Chiedo quanti anni abbia
+4 Creo una variabile con il costo al km del treno
+5 Faccio il calcolo tra il costo al km per i km dichiarati
+6 Creo una variabile per lo sconto
+7 Preparo il messaggio
+8 Creo la condizione scontistica se l'utente è minorenne
+9 Creo la condizione scontistica se l'utente è sopra i 65 anni
+10 Faccio il calcolo del prezzo del biglietto in base ai km dichiarati e agli anni dichiarati
+11 Preparo il messaggio con il prezzo scontato
+12 Presento il costo finale all'utente
--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log('JS OK')
+
+
+// 1 
+const prezzo = document.getElementById('prezzo')
+console.log('prezzo: ', prezzo)
+// 2
+const userKm = parseInt(prompt('Quanti chilometri vuoi fare?'))
+console.log('userKm: ', userKm)
+// 3
+const userAge = parseInt(prompt('Quanti anni hai?'))
+console.log('userAge: ', userAge)
+// 4 
+const fixedPrice = 0.21
+console.log('fixedPrice: ', fixedPrice)
+// 5
+let totalPrice = fixedPrice * userKm
+console.log('totalPrice: ', totalPrice)
+// 6
+let discountPrice = .20
+console.log('discountPrice: ', discountPrice)
+// 7
+let message = 'Il prezzo del tuo biglietto è: ' + totalPrice.toFixed(2) + ' €' 
+// 8, 9, 10, 11
+if (userAge < 18 ){
+    totalPrice += - (totalPrice * discountPrice)
+    console.log('totalPrice: ', totalPrice)  
+    message = 'Il prezzo del tuo biglietto è: ' + totalPrice.toFixed(2) + ' €' 
+} else if (userAge > 65) {
+    discountPrice = .40
+    totalPrice += - (totalPrice * discountPrice)
+    console.log('totalPrice: ', totalPrice) 
+    message = 'Il prezzo del tuo biglietto è: ' + totalPrice.toFixed(2) + ' €' 
+}
+// 12
+prezzo.innerText = message
